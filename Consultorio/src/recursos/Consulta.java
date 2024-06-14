ad50b4afe5e2580fd7b728d27425a318df82706c
AXMW <maxwilliamff@gmail.com>
2024-06-13 23:50:28 -0300

    adicionando a consulta e o paciente

---
+++ Consultorio/src/recursos/Consulta.java
@@ -0,0 +1,69 @@
+package recursos;
+
+public class Consulta {
+	private int id_Cons;
+	private String tipo_Consulta;
+	private String data_Consulta;
+	private Paciente paciente;
+	private Funcionario funcionario;
+	private Dentista dentista;
+	
+	public Consulta(int id_Cons, String tipo_Consulta, String data_Consulta, Paciente paciente, Funcionario funcionario, Dentista dentista) {
+		this.id_Cons = id_Cons;
+		this.tipo_Consulta = tipo_Consulta;
+		this.data_Consulta = data_Consulta;
+		this.paciente = paciente;
+		this.funcionario = funcionario;
+		this.dentista = dentista;
+	}
+
+	public int getId_Cons() {
+		return id_Cons;
+	}
+
+	public void setId_Cons(int id_Cons) {
+		this.id_Cons = id_Cons;
+	}
+
+	public String getTipo_Consulta() {
+		return tipo_Consulta;
+	}
+
+	public void setTipo_Consulta(String tipo_Consulta) {
+		this.tipo_Consulta = tipo_Consulta;
+	}
+
+	public String getData_Consulta() {
+		return data_Consulta;
+	}
+
+	public void setData_Consulta(String data_Consulta) {
+		this.data_Consulta = data_Consulta;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	public Dentista getDentista() {
+		return dentista;
+	}
+
+	public void setDentista(Dentista dentista) {
+		this.dentista = dentista;
+	}
+	
+	
+}
---
+++ Consultorio/src/recursos/Paciente.java
@@ -0,0 +1,75 @@
+package recursos;
+
+public class Paciente {
+	private int id_Pac;
+	private String nome_Pac;
+	private String endereco_Pac;
+	private String tipo_Sanguineo;
+	private String CPF_Pac;
+	private String telefone;
+	
+	public Paciente(int id_Pac, String nome_Pac, String endereco_Pac, String tipo_Sanguineo, String CPF_Pac, String telefone) {
+		this.id_Pac = id_Pac;
+		this.nome_Pac = nome_Pac;
+		this.endereco_Pac = endereco_Pac;
+		this.tipo_Sanguineo = tipo_Sanguineo;
+		this.CPF_Pac = CPF_Pac;
+		this.telefone = telefone;
+	}
+	public Paciente(String nome_Pac, String endereco_Pac, String tipo_Sanguineo, String CPF_Pac, String telefone) {
+		this.nome_Pac = nome_Pac;
+		this.endereco_Pac = endereco_Pac;
+		this.tipo_Sanguineo = tipo_Sanguineo;
+		this.CPF_Pac = CPF_Pac;
+		this.telefone = telefone;
+	}
+
+	public int getId_Pac() {
+		return id_Pac;
+	}
+
+	public void setId_Pac(int id_Pac) {
+		this.id_Pac = id_Pac;
+	}
+
+	public String getNome_Pac() {
+		return nome_Pac;
+	}
+
+	public void setNome_Pac(String nome_Pac) {
+		this.nome_Pac = nome_Pac;
+	}
+
+	public String getEndereco_Pac() {
+		return endereco_Pac;
+	}
+
+	public void setEndereco_Pac(String endereco_Pac) {
+		this.endereco_Pac = endereco_Pac;
+	}
+
+	public String getTipo_Sanguineo() {
+		return tipo_Sanguineo;
+	}
+
+	public void setTipo_Sanguineo(String tipo_Sanguineo) {
+		this.tipo_Sanguineo = tipo_Sanguineo;
+	}
+
+	public String getCPF_Pac() {
+		return CPF_Pac;
+	}
+
+	public void setCPF_Pac(String cPF_Pac) {
+		CPF_Pac = cPF_Pac;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+}
