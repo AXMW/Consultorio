18981cc32343e9bce2f853e43e7b8db52ee3794a
AXMW <maxwilliamff@gmail.com>
2024-06-14 04:31:42 -0300

    a

--- Consultorio/src/conexao/consulta/ReadConsulta.java
+++ Consultorio/src/conexao/consulta/ReadConsulta.java
@@ -24,20 +24,20 @@ public class ReadConsulta {
 		for(Consulta cons : consultas) {
 			String queryP = "select * from Paciente where ID_Pac = " + cons.getPaciente().getId_Pac();
 			ResultSet resultSet2 = statement.executeQuery(queryP);
-			resultSet2.next();
+			
 			Paciente p = new Paciente(resultSet2.getInt("ID_Pac"), resultSet2.getString("Nome_Pac"), 
 					resultSet2.getString("Endereco_Pac"), resultSet2.getString("Tipo_Sanguineo"),
 					resultSet2.getString("CPF_Pac"), resultSet2.getString("Telefone"));
 			cons.setPaciente(p);
 			String queryF = "select * from Funcionario where ID_Func = " + resultset.getInt("ID_Func");
 			ResultSet resultSet3 = statement.executeQuery(queryF);
-			resultSet3.next();
+			
 			Funcionario f = new Funcionario(resultSet3.getInt("ID_Func"), resultSet3.getString("Nome_Func"),
 					resultSet3.getString("Login"), resultSet3.getString("Senha"), resultSet3.getBoolean("Gerente"));
 			cons.setFuncionario(f);
 			String queryD = "select * from Dentista where ID_Dent = " + resultset.getInt("ID_Dent");
 			ResultSet resultSet4 = statement.executeQuery(queryD);
-			resultSet4.next();
+			
 			Dentista d = new Dentista(resultSet4.getInt("ID_Dent"), resultSet4.getString("Nome_Dent"), 
 			resultSet4.getString("CRM"), resultSet4.getString("Especialidade"));
 			cons.setDentista(d);
