9cb0d8f9f79f0afb8d4125629b7476bf423e7bfa
AXMW <maxwilliamff@gmail.com>
2024-06-14 05:09:23 -0300

    update paciente

--- Consultorio/src/conexao/paciente/UpdatePaciente.java
+++ Consultorio/src/conexao/paciente/UpdatePaciente.java
@@ -8,7 +8,8 @@ public class UpdatePaciente {
 		Statement statement = con.createStatement();
 		String query = "update paciente set Nome_Pac = '" + paciente.getNome_Pac() + "', Endereco_Pac = '"
 				+ paciente.getEndereco_Pac() + "', Tipo_Sanguineo = '" + paciente.getTipo_Sanguineo() + 
-				"', CPF_Pac = '" + paciente.getCPF_Pac() + "' where ID_Pac = " + paciente.getId_Pac() +";";
+				"', CPF_Pac = '" + paciente.getCPF_Pac() + "', Telefone = '" + paciente.getTelefone() + 
+				"' where ID_Pac = " + paciente.getId_Pac() +";";
 		statement.executeUpdate(query);
 		
 	}
