597ad7c8e44720412dfecb0701ffa2dd295667a4
AXMW <maxwilliamff@gmail.com>
2024-06-14 03:55:55 -0300

    update funcionario

--- Consultorio/src/conexao/funcionario/UpdateFuncionario.java
+++ Consultorio/src/conexao/funcionario/UpdateFuncionario.java
@@ -8,9 +8,10 @@ import recursos.Funcionario;
 public class UpdateFuncionario {
 	public static void update(Connection con, Funcionario funcionario) throws Exception {
 		Statement statement = con.createStatement();
+		int gerente = funcionario.isGerente() ? 1 : 0;
 		String query = "update Funcionario set Nome_Func = '" + funcionario.getNome_Func() + "', Login = '"
 				+ funcionario.getLogin() + "', Senha = '" + funcionario.getSenha() + 
-				"', Gerente = '" + funcionario.isGerente() + "' where ID_Func = " + funcionario.getId_Func() + ";";
+				"', Gerente = '" + gerente + "' where ID_Func = " + funcionario.getId_Func() + ";";
 		statement.executeUpdate(query);
 		
 	}
